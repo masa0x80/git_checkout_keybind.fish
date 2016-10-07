7c78de3932415edce315ed4e212d3cd36b0c84d3
Kimura Masayuki <masa0x80@gmail.com>
2016-10-07 12:31:46 +0900

    Use `angler` -- interactive filtering tool wrapper

--- functions/__select_from_all_recent_branches.fish
+++ functions/__select_from_all_recent_branches.fish
@@ -1,7 +1,7 @@
 function __select_from_all_recent_branches
   commandline | read -l buffer
   git for-each-ref --format='%(refname)' --sort=-committerdate | \
-        fzf | read -l selected_branch
+        angler | read -l selected_branch
   if test -n "$selected_branch"
     if test -n "$buffer"
       commandline -i (echo $selected_branch | sed -e 's|^refs\/heads\/||' -e 's|^refs\/remotes\/origin\/||')
--- functions/__select_from_recent_branches.fish
+++ functions/__select_from_recent_branches.fish
@@ -2,7 +2,7 @@ function __select_from_recent_branches
   commandline | read -l buffer
   git for-each-ref --format='%(refname)' --sort=-committerdate refs/heads | \
         sed -e 's|^refs/heads/||' | \
-        fzf | read -l selected_branch
+        angler | read -l selected_branch
   if test -n "$selected_branch"
     if test -n "$buffer"
       commandline -i $selected_branch

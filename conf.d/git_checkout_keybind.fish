357148972203d13afcbff98d5f01d378742e0c9f
KIMURA Masayuki <masa0x80@gmail.com>
2018-01-24 22:16:20 +0900

    Fix __register_git_checkout_keybind

--- conf.d/git_checkout_keybind.fish
+++ conf.d/git_checkout_keybind.fish
@@ -1,7 +1,10 @@
-function __register_git_checkout_keybind --on-event fish_prompt
-  not set -q SELECT_BRANCH_KEYBIND; and set -l SELECT_BRANCH_KEYBIND '\cg\cb'
-  not set -q SELECT_BRANCH_ALL_KEYBIND; and set -l SELECT_BRANCH_ALL_KEYBIND '\cg\cg\cb'
-  eval bind $SELECT_BRANCH_KEYBIND __select_from_recent_branches
-  eval bind $SELECT_BRANCH_ALL_KEYBIND __select_from_all_recent_branches
-  functions -e __register_git_checkout_keybind
+function __register_git_checkout_keybind
+    not set -q SELECT_BRANCH_KEYBIND
+    and set -l SELECT_BRANCH_KEYBIND '\cg\cb'
+    not set -q SELECT_BRANCH_ALL_KEYBIND
+    and set -l SELECT_BRANCH_ALL_KEYBIND '\cg\cg\cb'
+    eval bind $SELECT_BRANCH_KEYBIND __select_from_recent_branches
+    eval bind $SELECT_BRANCH_ALL_KEYBIND __select_from_all_recent_branches
+    functions -e __register_git_checkout_keybind
 end
+__register_git_checkout_keybind
